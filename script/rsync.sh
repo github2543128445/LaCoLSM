5646b3a5286f26dab84780d31a8e061d3410312b
victor <2543128445@qq.com>
2025-07-10 19:51:03 +0800

    about script

--- script/rsync.sh
+++
@@ -1,8 +0,0 @@
-rsync -av --exclude=build/ --exclude=data/ --exclude=script/run-MN.sh --exclude=*.csv ~/louzy/LaCoLSM/ skv-node1:~/louzy/LaCoLSM
-rsync -av --exclude=build/ --exclude=data/ --exclude=script/run-MN.sh --exclude=*.csv ~/louzy/LaCoLSM/ skv-node2:~/louzy/LaCoLSM
-rsync -av --exclude=build/ --exclude=data/ --exclude=script/run-MN.sh --exclude=*.csv ~/louzy/LaCoLSM/ skv-node4:~/louzy/LaCoLSM
-rsync -av --exclude=build/ --exclude=data/ --exclude=script/run-MN.sh --exclude=*.csv ~/louzy/LaCoLSM/ skv-node5:~/louzy/LaCoLSM
-rsync -av --exclude=build/ --exclude=data/ --exclude=script/run-MN.sh --exclude=*.csv ~/louzy/LaCoLSM/ skv-node6:~/louzy/LaCoLSM
-rsync -av --exclude=build/ --exclude=data/ --exclude=script/run-MN.sh --exclude=*.csv ~/louzy/LaCoLSM/ skv-node7:~/louzy/LaCoLSM
-cd ../build
-make Server db_bench TimberSaw
---
+++ script/sync.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# 优化后的同步编译脚本
+# 用法：./sync_build.sh [本节点编号]
+# 示例：./sync_build.sh 2  # 表示当前节点是skv-node2
+
+if [ $# -ne 1 ]; then
+  echo "错误：请提供本节点编号（1-7）"
+  echo "示例：$0 2"
+  exit 1
+fi
+
+current_node=$1
+nodes=(1 2 3 4 5 6 7)  # 所有节点编号
+
+# 同步到其他节点（排除自身）
+for node in "${nodes[@]}"; do
+  if [ "$node" -ne "$current_node" ]; then
+    echo "正在同步到 skv-node$node..."
+    rsync -av \
+      --exclude=build/ \
+      --exclude=data/ \
+      --exclude=script/run-MN.sh \
+      --exclude=*.csv \
+      ~/louzy/LaCoLSM/ \
+      skv-node$node:~/louzy/LaCoLSM
+  fi
+done
+
+# 编译项目
+cd ../build
+make Server db_bench TimberSaw
